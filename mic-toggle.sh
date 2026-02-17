2d07c7e2a9f4931e065a6dc3d64b66aa98294af3
amller <arthurmueller95@gmail.com>
2026-02-17 20:54:41 +0100

    Use sh instead of bash

--- mic-toggle.sh
+++ mic-toggle.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 DIR=$(dirname "$0")
 STATUS=$(amixer get Capture | tail --lines=1 | grep --fixed-strings --only-matching "[on]")
 test "$STATUS" = "[on]" && AUDIOFILE="mute.wav" || AUDIOFILE="unmute.wav"
